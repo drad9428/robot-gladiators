32001cd0242bd9f981652991ae956a66f26a1193
drad <robertjdstoddard@gmail.com>
2021-05-03 16:15:12 -0600

    add first round

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,38 @@
-window.alert("This is an alert! JavaScript is running!");
-
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
-console.log(10 + 10);
-console.log("Our robot's name is " + playerName);
-var name = "Robert";
-console.log(name);
+//player stats
+console.log(playerName, playerAttack, playerHealth);
 
-function fight(){
-    window.alert("The fight has begun");
-}
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-// fight();
+var fight = function(){
+    window.alert("Welcome to Robot Gladiator's!");
+
+    //player attack
+    enemyHealth = enemyHealth - playerAttack;
+    console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
+    
+    if (enemyHealth <= 0){
+        window.alert(enemyName + " has died");
+    }
+    else{
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    //enemy attack
+    playerHealth = playerHealth - enemyAttack;
+    console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+    if (playerHealth <= 0){
+        window.alert(playerName + " has died");
+    }
+    else{
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+};
+
+fight();
