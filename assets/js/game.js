0908f8e789b2bb3b3fe731bf9b3fda84268c54d2
drad <robertjdstoddard@gmail.com>
2021-05-06 13:25:14 -0600

    random health and attack

--- assets/js/game.js
+++ assets/js/game.js
@@ -18,13 +18,14 @@ var fight = function(enemyName){
 
       if(confirmSkip){
         window.alert(playerName + ' has decided to skip this fight. Goodbye!');
-        playerMoney = playerMoney - 10;
+        playerMoney = Math.max(0, playerMoney - 10);
         console.log("playerMoney", playerMoney)
         break;
       }
     }
 
-    enemyHealth = enemyHealth - playerAttack;
+    var damage = randomNumber(playerAttack - 3, playerAttack);
+    enemyHealth = Math.max(0, enemyHealth - damage);
     console.log(playerName + ' attacked ' + enemyName + '. ' + enemyName + ' now has ' + enemyHealth + ' health remaining.');
 
     if(enemyHealth <= 0){
@@ -38,7 +39,8 @@ var fight = function(enemyName){
       window.alert(enemyName + ' still has ' + enemyHealth + ' health left.');
     }
 
-    playerHealth = playerHealth - enemyAttack;
+    var damage = randomNumber(enemyAttack - 3, enemyAttack);
+    playerHealth = Math.max(0, playerHealth - damage);
     console.log(enemyName + ' attacked ' + playerName + '. ' + playerName + ' now has ' + playerHealth + ' health remaining.');
 
     if(playerHealth <= 0){
@@ -74,16 +76,29 @@ var shop = function(){
 
   switch(shopOptionPrompt){
     case "REFILL":
-      window.alert("Refilling player's health by 20 for 7 dollars.")
-      playerHealth = playerHealth + 20;
-      playerMoney = playerMoney - 7;
+    case "refill":
+      if(playerMoney >= 7){
+        window.alert("Refilling player's health by 20 for 7 dollars.");
+        playerHealth = playerHealth + 20;
+        playerMoney = playerMoney - 7;
+      }
+      else{
+        window.alert("You don't have enough money!");
+      }
       break;
     case "UPGRADE":
-      window.alert("Upgrading player's attack by 6 for 7 dollars.");
-      playerAttack = playerAttack + 6;
-      playerMoney = playerMoney - 7;
+    case "upgrade":
+      if(playerMoney >= 7){
+        window.alert("Upgrading player's attack by 6 for 7 dollars.");
+        playerAttack = playerAttack + 6;
+        playerMoney = playerMoney - 7;
+      }
+      else{
+        window.alert("You don't have enough money!");
+      }
       break;
     case "LEAVE":
+    case "leave":
       window.alert("Leaving the store.");
       break;
     default:
@@ -91,6 +106,11 @@ var shop = function(){
       shop();
       break;
   }
+}
+
+var randomNumber = function(min, max){
+  var value = Math.floor(Math.random() * (max - min + 1) + min);
+  return value;
 }
 
 var startGame = function(){
@@ -102,7 +122,7 @@ var startGame = function(){
     if(playerHealth > 0){
       window.alert("Welcome to Robot Gladiators! Round: " + (i + 1));
       var pickedEnemyName = enemyNames[i];
-      enemyHealth = 50;
+      enemyHealth = randomNumber(40, 60);
       fight(pickedEnemyName);
 
       if(playerHealth > 0 && i < enemyNames.length - 1){
